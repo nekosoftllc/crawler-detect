88d8ca9761c177a4f77b3cc79188d8d437515168
Federico Mestrone <federico@moodsdesign.com>
2022-09-01 22:36:16 +0100

    Refactoring, fixes, and documentation for version 1.0.0
    Added javadoc to all public interfaces
    Fixed Headers data loading: needed lowercasing and replacing _  with -
    Added tests for getMatchingCrawler with headers
    Hard-coded PHP project URLs into Crawlers, Headers and Exclusions classes
    Adjusted README.md to the above changes

--- src/main/java/org/nekosoft/utils/crawlerdetect/data/Exclusions.java
+++ src/main/java/org/nekosoft/utils/crawlerdetect/data/Exclusions.java
@@ -1,19 +1,25 @@
 package org.nekosoft.utils.crawlerdetect.data;
 
 import org.nekosoft.utils.crawlerdetect.AbstractDataProvider;
+import org.nekosoft.utils.crawlerdetect.Detector;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 
+/**
+ * This {@link AbstractDataProvider} is preconfigured to load User Agent Exclusions (see {@link Detector#getUaExclusions()})
+ * from the <a href="https://crawlerdetect.io">Github repository</a> of the original PHP project.
+ */
 public class Exclusions extends AbstractDataProvider {
 
-    public Exclusions(URL source) throws IOException {
-        super(source);
-    }
-
-    public Exclusions(List<String> data) {
-        super(data);
+    /**
+     * Creates a new instance of this {@link AbstractDataProvider} that loads data from the
+     * <a href="https://raw.githubusercontent.com/JayBizzle/Crawler-Detect/master/raw/Exclusions.txt">Github repository</a>
+     * of the original PHP project
+     * @throws IOException if there were issues downloading data from the source URL
+     */
+    public Exclusions() throws IOException {
+        super(new URL("https://raw.githubusercontent.com/JayBizzle/Crawler-Detect/master/raw/Exclusions.txt"));
     }
 
 }
--- src/main/java/org/nekosoft/utils/crawlerdetect/data/Headers.java
+++ src/main/java/org/nekosoft/utils/crawlerdetect/data/Headers.java
@@ -1,27 +1,36 @@
 package org.nekosoft.utils.crawlerdetect.data;
 
 import org.nekosoft.utils.crawlerdetect.AbstractDataProvider;
+import org.nekosoft.utils.crawlerdetect.Detector;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 
+/**
+ * This {@link AbstractDataProvider} is preconfigured to load Headers To Check (see {@link Detector#getHeadersToCheck()})
+ * from the <a href="https://crawlerdetect.io">Github repository</a> of the original PHP project.
+ * <p>
+ * This class needs to make some changes to the original data to account for differences in the way
+ * that Java and PHP access HTTP header information (see {@link #reloadData()}).
+ */
 public class Headers extends AbstractDataProvider {
 
-    public Headers(URL source) throws IOException {
-        super(source);
-    }
-
-    public Headers(List<String> data) {
-        super(data);
+    /**
+     * Creates a new instance of this {@link AbstractDataProvider} that loads data from the
+     * <a href="https://raw.githubusercontent.com/JayBizzle/Crawler-Detect/master/raw/Headers.txt">Github repository</a>
+     * of the original PHP project.
+     * @throws IOException if there were issues downloading data from the source URL
+     */
+    public Headers() throws IOException {
+        super(new URL("https://raw.githubusercontent.com/JayBizzle/Crawler-Detect/master/raw/Headers.txt"));
     }
 
     /**
      * This class needs to make some changes to the original data to account for differences in the way
      * that Java and PHP access HTTP header information.
      * <p>
-     * The reloadData method strips every value of the HTTP_ prefix, replaces _ with -, and turns the string
-     * to lower case. E.g.
+     * The {@code reloadData} method strips every value of the {@code HTTP_} prefix, replaces {@code _} with {@code -},
+     * and turns the string to lower case. E.g.
      * <pre>
      *     "HTTP_USER_AGENT" becomes "user-agent"
      * </pre>
